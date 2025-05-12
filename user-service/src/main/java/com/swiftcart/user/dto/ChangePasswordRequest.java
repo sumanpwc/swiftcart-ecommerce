79adc132dec0fc135916ee770200047c6dba52a7
Suman_java_exp <113517558+sumanpwc@users.noreply.github.com>
2025-05-13 02:09:05 +0530

    Update ChangePasswordRequest.java

--- user-service/src/main/java/com/swiftcart/user/dto/ChangePasswordRequest.java
+++ user-service/src/main/java/com/swiftcart/user/dto/ChangePasswordRequest.java
@@ -1,18 +1,5 @@
 package com.swiftcart.user.dto;
 
 public class ChangePasswordRequest {
-    private String currentPassword;
-    private String newPassword;
-	public String getCurrentPassword() {
-		return currentPassword;
-	}
-	public void setCurrentPassword(String currentPassword) {
-		this.currentPassword = currentPassword;
-	}
-	public String getNewPassword() {
-		return newPassword;
-	}
-	public void setNewPassword(String newPassword) {
-		this.newPassword = newPassword;
-	}
+
 }
