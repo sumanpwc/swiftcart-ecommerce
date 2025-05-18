aa571a7d559be5d221e76cdd11a4fbdf370dacd5
Suman <hysuman55@gmail.com>
2025-05-18 18:06:26 +0530

    Controller and Service Class Updated

--- user-service/src/main/java/com/swiftcart/user/controller/PublicController.java
+++ user-service/src/main/java/com/swiftcart/user/controller/PublicController.java
@@ -1,7 +1,5 @@
 package com.swiftcart.user.controller;
 
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,32 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.swiftcart.user.config.UserPrincipal;
 import com.swiftcart.user.dto.JwtResponse;
+import com.swiftcart.user.dto.RegisterRequest;
 import com.swiftcart.user.dto.UserRequest;
-import com.swiftcart.user.dto.UserResponse;
-import com.swiftcart.user.model.User;
-import com.swiftcart.user.repository.UserRepository;
 import com.swiftcart.user.service.JwtTokenUtilService;
 import com.swiftcart.user.service.UserDetailsServiceImpl;
 import com.swiftcart.user.service.UserService;
 
-
-
 @RestController
-@RequestMapping("/api/v1/public")
 public class PublicController {
 	
 	private static final Logger log = LoggerFactory.getLogger(PublicController.class);
-	
-	@Autowired
-	private UserRepository userRepository;
 	
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
@@ -49,7 +37,17 @@ public class PublicController {
 	@Autowired
 	UserService userService;
 	
-	@PostMapping("/generateToken")
+	// Register New User
+	
+	@PostMapping("/api/v1/auth/register")
+	public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	// Login and Get JWT Token
+	
+	@PostMapping("/api/v1/auth/login")
 	public ResponseEntity<?> generateToken (@RequestBody UserRequest userRequest) {
 		log.info("=================Public Controller 1===================" + userRequest.getEmail() + "==================" + userRequest.getPassword());
 		
@@ -77,11 +75,16 @@ public class PublicController {
 		
 	}
 	
-	@GetMapping("/find")
-	public ResponseEntity<?> getUser(@RequestBody UserRequest userRequest) {
-	    Optional<User> user = userRepository.findByEmail(userRequest.getEmail());
-	    UserResponse userResponse = userService.convertToUserResponseDTO(user.get());
-	    
-	    return new ResponseEntity<>(userResponse, HttpStatus.OK);
+	// Refresh Token 
+	
+	@PostMapping("/api/v1/auth/token/refresh")
+	public ResponseEntity<?> refreshToken(@RequestBody UserRequest userRequest){
+		
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	// OAuth2 login (Google, GitHub, etc.)
+	//@GetMapping("/oauth2/authorize/{provider}")
+	
+	
 }
--- user-service/src/main/java/com/swiftcart/user/controller/UserController.java
+++ user-service/src/main/java/com/swiftcart/user/controller/UserController.java
@@ -1,5 +1,71 @@
 package com.swiftcart.user.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swiftcart.user.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
+	// Greetings User
+	
+    @GetMapping("/hello")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public String greetings() {   	
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	String email = authentication.getName();
+    	String username = userService.findUserByEmail(email).getFullName();
+    	return "Welcome " + username;
+    }
+    
+    // View Profile 
+    
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> viewProfile(){
+    	
+    	return new ResponseEntity<> (HttpStatus.OK);
+    }
+    
+    // Update Profile
+    
+    @PutMapping("/me")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> updateProfile(){
+    	
+    	return new ResponseEntity<> (HttpStatus.OK);
+    }
+    
+    // Change Password
+    
+    @PostMapping("me/password")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> changePassword(){
+    	
+    	return new ResponseEntity<> (HttpStatus.OK);
+    }
+    
+    // Logout
+    
+    @PostMapping("/logout")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> logout(){
+    	
+    	return new ResponseEntity<> (HttpStatus.OK);
+    }
 }
